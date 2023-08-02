c6d395b7a3eb13dd7c8065011fbbe3b051fe2407
andrefcordeiro <andrefc36@gmail.com>
2023-08-01 23:58:41 -0300

    Separando funções das páginas em arquivos .js

---
+++ frontend/src/js/breakfast-registration.js
@@ -0,0 +1,28 @@
+function registerBreakfast(event) {
+    event.preventDefault()
+
+    var breakfast = {}
+    breakfast['date'] = $('#register-breakfast-date').val()
+    var body = JSON.stringify(breakfast)
+    $.ajax({
+        url: "http://localhost:8080/breakfast",
+        contentType: "application/json",
+        data: body,
+        dataType: "json",
+        type: 'POST',
+        success: function (response) {
+            $("#out-form").text('Café da manhã cadastrado')
+            $("#out-form").css('color', 'green')
+        },
+        error:  function (response) {
+            if(response.responseText.includes('Resource already exists. date:')){
+                $("#out-form").text('Café da manhã já cadastrado para esta data')
+                $("#out-form").css('color', 'red')
+            }
+            else {
+                $("#out-form").text(response.responseText)
+                $("#out-form").css('color', 'red')
+            } 
+        },
+    })
+}
---
+++ frontend/src/js/employee-registration.js
@@ -0,0 +1,29 @@
+function registerEmployee(event) {
+    event.preventDefault()
+    var employee = {} 
+    employee['name'] = $('#register-employee-name').val()
+    employee['cpf'] = $('#register-employee-cpf').val()
+
+    var body = JSON.stringify(employee)
+    $.ajax({
+        url: "http://localhost:8080/employee",
+        contentType: "application/json",
+        data: body,
+        dataType: "json",
+        type: 'POST',
+        success: function (response) {
+            $("#out-form").text('Colaborador cadastrado')
+            $("#out-form").css('color', 'green')
+        },
+        error:  function (response) {
+            if(response.responseText.includes('Resource already exists. cpf:')){
+                $("#out-form").text('Colaborador com este CPF já cadastrado')
+                $("#out-form").css('color', 'red')
+            }
+            else {
+                $("#out-form").text(response.responseText)
+                $("#out-form").css('color', 'red')
+            } 
+        },
+  })
+}
